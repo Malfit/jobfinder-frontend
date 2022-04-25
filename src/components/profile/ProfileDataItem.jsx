b155b5971d81e0e9c2e381b78e8822f0b7c59140
Aleksandr <oleksandr.rybakov@nure.ua>
2022-04-25 12:59:51 +0300

    Update ProfileDataItem.jsx

--- src/components/profile/ProfileDataItem.jsx
+++ src/components/profile/ProfileDataItem.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-export const ProfileDataItem = (props) => {
+export const ProfileDataItem = (props) => { // use destructurization = ({ field, color, date }) => {
     return (
         <div>
             <h2>Your {props.field}: <span style={{color: props.color}}>{props.data}</span></h2>
