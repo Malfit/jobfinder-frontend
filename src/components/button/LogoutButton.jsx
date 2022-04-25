7a85902f229798d63679a88441224353711df32d
Aleksandr <oleksandr.rybakov@nure.ua>
2022-04-25 12:53:02 +0300

    Update LogoutButton.jsx

--- src/components/button/LogoutButton.jsx
+++ src/components/button/LogoutButton.jsx
@@ -3,12 +3,13 @@ import {Button} from "@mui/material";
 import {useNavigate} from "react-router-dom";
 
 export const LogoutButton = () => {
-    let handleRedirect = useNavigate();
+    let handleRedirect = useNavigate(); // const 
 
     const handleLogout = () => {
         localStorage.removeItem('Authorization')
         handleRedirect('/login')
     }
+    // + white space
     return (
         <div>
             <Button onClick={handleLogout}>Logout</Button>
